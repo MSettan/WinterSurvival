a1c79ea6c80b7c5be9c24b9a113b4759fa730695
Settan <cntg4br2000@mail.ru>
2026-05-25 23:29:26 +0300

    Update background.png, background.png.import and thirty-six other files

--- WinterSurvival/src/Actors/Actor.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-class_name Actor
-
-@export var gravity: = 3000.0
-
-func _physics_process(_delta: float) -> void:
-	velocity = Vector2(300,0)
-	velocity.y += gravity * _delta
-	move_and_slide() 
